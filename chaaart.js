8fe534c6cec54063046fd2abe70df8ba82a5a62d
Ricardo Andrés Bello <ricardoaandres@me.com>
2015-04-23 01:18:34 -0300

    Add bugs

--- chaaart.js
+++ chaaart.js
@@ -19,8 +19,9 @@ function saveImage (chart) {
       imageData += chunk;
     });
 
+    console.log(program.output);
     response.on('end', function () {
-      fs.writeFile('holitas.png', imageData, 'binary', function (error) {
+      fs.writeFile(program.output, imageData, 'binary', function (error) {
         if (error) {
           throw error;
         }
@@ -30,36 +31,48 @@ function saveImage (chart) {
 }
 
 function initialize () {
-  var pkg = require(path.join(__dirname, 'package.json'));
+  var pkg = require(path.join(__dirname, 'package.json')),
+      errorMessage = 'Missing %s. Type --help for documentation';
+
+  program.on('--help', function () {
+    console.log('  Example:');
+    console.log('');
+    console.log('    $ chaaart --chart line --title awesome --output chart.png');
+    console.log('    $ chaaart -c line -t awesome -o chart.png');
+    console.log('');
+  });
 
   program
     .version(pkg.version)
-    .option('-t, --title', 'Add title', '')
-    .option('-c, --chart', 'Add chart type [line]')
-    .option('-o, --output', 'Output file name [chart.png]', 'chart.png')
+    .option('-t, --title', 'Add chart title')
+    .option('-c, --chart', 'Add chart type')
+    .option('-o, --output', 'Output file name')
     .parse(process.argv);
 
   if (!program.chart) {
-    chalk.red('Missing chart type. Type --help for documentation');
-    return false;
+    console.log(chalk.red(errorMessage), 'chart type');
+    process.exit(1);
   }
 
   if (!program.output) {
-    chalk.red('Missing output path. Type --help for documentation');
-    return false;
+    console.log(chalk.red(errorMessage), 'output path');
+    process.exit(1);
   }
 }
 
 +function () {
   'use strict';
 
-  var chart = quiche('line'),
-      months = ['', 'Ene', 'Feb', 'Mar', 'Abr', 'May', 'Jun', 'Jul', 'Ago', 'Sep', 'Nov', 'Dic'];
+  var chart, months;
 
   initialize();
 
+  console.log(program);
 
-  chart.setTitle(program.title);
+  chart = quiche(program.chart);
+  months = ['', 'Ene', 'Feb', 'Mar', 'Abr', 'May', 'Jun', 'Jul', 'Ago', 'Sep', 'Nov', 'Dic'];
+
+  // chart.setTitle(program.title);
   chart.addData([1000, 1500, 2000, 1300, 5000, 4300, 800, 1000, 2500, 2200, 2000, 2000], 'kWh', '4e4e99');
   chart.addAxisLabels('x', months);
   chart.setWidth(430);
